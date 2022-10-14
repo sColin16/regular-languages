2771c7e39085bc70d22b081be91c0de0db2eddef
Colin Siles <colin.siles16@gmail.com>
2022-10-14 17:01:04 -0600

    DFAs use function composition instead of explicit maps

--- src/DFA/dfa.py
+++ src/DFA/dfa.py
@@ -1,5 +1,7 @@
-from collections import defaultdict
+from collections.abc import Iterable
 from dataclasses import dataclass
+from enum import Enum, auto
+from itertools import product
 from typing import Callable, Dict, Generic, List, Set, TypeAlias, TypeVar
 
 T = TypeVar('T')
@@ -8,6 +10,14 @@ U = TypeVar('U')
 TransitionFunction: TypeAlias = Callable[[T, U], T] 
 TransitionMap: TypeAlias = Dict[T, Dict[U, T]]
 TransitionList: TypeAlias = List[Dict[U, int]]
+
+class State(Enum):
+    '''
+    An helper enum to internally define dead states. This should be treated as
+    internal and should not be used to define DFA states externally
+    '''
+
+    DEAD = auto()
 
 @dataclass
 class DFA(Generic[T, U]):
@@ -18,100 +28,84 @@ class DFA(Generic[T, U]):
 
     states: Set[T]
     alphabet: Set[U]
-    transition_map: TransitionMap
+    transition_function: TransitionFunction
     start_state: T
     accept_states: Set[T]
 
     def __post_init__(self):
         '''
-        Verifies that the constraints on the components of the DFA are met
+        Verifies that the constraints on the components of the DFA are met.
+
+        Note that this function cannot verify that the transition function is
+        "safe" (i.e. that it is only defined for the set of valid state/symbol
+        combinations). Use the unsafe constructor helper to guarantee that
         '''
 
-        assert self.start_state in self.states
-        assert self.accept_states.issubset(self.states)
+        if self.start_state not in self.states:
+            raise Exception(f'Start state {self.start_state} is not in the set of states')
 
-        for state in self.states:
-            if state not in self.transition_map:
-                raise Exception(f'State {state} is missing from the domain of ' +
-                                'the DFA\'s transition function')
+        if not self.accept_states.issubset(self.states):
+            raise Exception('The accept states are not a subset of the valid states')
 
-            for symbol in self.alphabet:
-                if symbol not in self.alphabet:
-                    raise Exception(f'Symbol {symbol} is missing from the domain of ' +
-                                    'the DFA\'s transition function')
-
-                if self.transition_map[state][symbol] not in self.states:
-                    raise Exception('DFA\'s transition function returned state '+
-                                    f'{state}, which is not a valid state')
+        for state, symbol in product(self.states, self.alphabet):
+            if self.transition_function(state, symbol) not in self.states:
+                raise Exception(f'Transition function returned state {state}, ' +
+                                'which is not a valid state')
 
     @classmethod
-    def from_transition_function(cls, states: Set[T], alphabet: Set[U],
-                                 transition_function: TransitionFunction,
-                                 start_state: T, accept_states: Set[T]):
+    def from_unsafe_transition_func(cls, states: Set[T], alphabet: Set[U],
+                                    unsafe_transition_func: TransitionFunction,
+                                    start_state: T, accept_states: Set[T]):
         '''
-        Constructs a DFA from an arbitrary transition function. Note that the
-        states and alphabet passed will control what is considered valid by the
-        DFA, even if the function accepts other states and/or symbols
+        Constructs a DFA from an unsafe transition function, a transition
+        function that may be defined for state/symbol combinations that are not
+        considered valid for the DFA, by wrapping the function in checks that
+        the arguments are in the true domain. Provides an "upper bound" on the
+        transition function type
         '''
 
-        transition_map = defaultdict(dict)
+        def safe_transition_func(state: T, symbol: U) -> T:
+            if state not in states:
+                raise Exception(f'{state} is not a valid state')
 
-        for state in states:
-            for symbol in alphabet:
-                next_state = transition_function(state, symbol)
+            if symbol not in alphabet:
+                raise Exception(f'{symbol} is not in the alphabet')
 
-                transition_map[state][symbol] = next_state
+            return unsafe_transition_func(state, symbol)
 
-        return cls(states, alphabet, transition_map, start_state, accept_states)
+        return cls(states, alphabet, safe_transition_func, start_state, accept_states)
 
     @classmethod
     def from_transition_map(cls, transition_map: TransitionMap, start_state: T,
                             accept_states: Set[T]):
         '''
-        Construct a DFA from a transition map, which may be implicit. States
-        that have no defined transitions are considered dead states, and
-        symbols in the alphabet that are missing from a state's transitions are
-        considered transitions to a dead state
+        Constructs a DFA from a transition map, a nested dictionary that
+        implictly defines the alphabet and set of states, and need not
+        explicitly define the transition for every state symbol combination.
+        Non-explicit transitions are considred transitions to a "dead state"
         '''
 
-        # TODO: handle a None state being passed, or document that
-        # TODO: only create the None state if it's needed
-        # TODO: try to consolidate the dead states if possible
+        # TODO: can I detect if I need the dead state here?
+        # I do think dead state consolidation is the job of the minimize function
+        # not sure if eliminating this state is necessary - I lean no
 
-        states = set([None])
-        alphabet = set()
-        final_transition_map = defaultdict(dict)
+        # Unpack the states and alphabet implied in the transition map
+        states = set(transition_map.keys())\
+                    .union(*(transitions.values() for transitions in transition_map.values()))\
+                    .union({State.DEAD})
+        alphabet = set().union(*(transitions.keys() for transitions in transition_map.values()))
 
-        # First, construct the pieces of the transition map we do have
-        for state in transition_map:
-            states.add(state)
+        def unsafe_transition_func(state: T, symbol: U) -> T:
+            if state is State.DEAD or state not in transition_map:
+                return State.DEAD
 
-            for symbol in transition_map[state]:
-                alphabet.add(symbol)
+            if symbol not in transition_map[state]:
+                return State.DEAD
 
-                next_state = transition_map[state][symbol]
+            return transition_map[state][symbol]
 
-                final_transition_map[state][symbol] = next_state
-                states.add(next_state)
-
-        # Create a dead state for the DFA, which may not be necessary
-        final_transition_map[None] = {symbol: None for symbol in alphabet}
-
-        # Then, take another pass and fill in dead states and dead state transitions
-        for state in states:
-            # This is a dead state if the state had no defined transitions
-            # So fill in all the transitions to loop back to this state
-            if state not in final_transition_map:
-                final_transition_map[state] = {symbol: state for symbol in alphabet}
-                continue
-
-            # Otherwise, direct all undefined transitions for the state to the dead state
-            for symbol in alphabet:
-                if symbol not in final_transition_map[state]:
-                    final_transition_map[state][symbol] = None
-
-
-        return cls(states, alphabet, final_transition_map, start_state, accept_states)
+        return cls.from_unsafe_transition_func(states, alphabet, unsafe_transition_func,
+                                               start_state, accept_states)
 
     @classmethod
     def from_transition_list(cls, transition_list: TransitionList,
@@ -133,56 +127,12 @@ class DFA(Generic[T, U]):
     #     Constructs a DFA that recognizes the same language as the provided NFA
     #     '''
 
-    #     # Basically just modify the transition map to return sets
+    #     # Use subset construction to do this, possibly creating an intermediate
+    #     # NFA that removes the epsilon transitions
 
     #     pass
 
-    def get_transition_list(self):
-        '''
-        This would be an interesting function to rename the states so that we
-        could get a super interopable DFA, even for strange states. This would
-        be applicable for some of the conversions between DFA types!
-        '''
-
-        # TODO: should this be implicit or not? A flag you can pass? Two
-        # separate functions?
-        # TODO: ideally we would rename states so that state 1 has a transition
-        # from state 0, and build out using BFS to get interpretable state ids
-
-        pass
-
-    def get_implicit_transition_map(self):
-        '''
-        This would return a simplified transition map that essentially undoes
-        what the constructor helper for this does, and removes all references
-        to the dead state. Good for printing out and understanding DFAs
-        '''
-
-        # TODO: I must detect all dead states
-        pass
-
-    def __post_init__(self):
-        '''
-        Verifies that the constraints on the components of the DFA are met
-        '''
-
-        assert self.start_state in self.states
-        assert self.accept_states.issubset(self.states)
-
-        # TODO: check the domain and range of the transition map match the
-        # domain and range define by the state and alphabet
-        # We want to do this to check the helper constructors and as 
-
-    def transition_function(self, state: T, symbol: U) -> T:
-        if state not in self.states:
-            raise Exception(f'Error computing transition: {state} is not a valid state')
-
-        if symbol not in self.alphabet:
-            raise Exception(f'Error computing transition: {symbol} is not in the alphabet')
-
-        return self.transition_map[state][symbol]
-
-    def simulate(self, test_string: List[U], start_state=None) -> T:
+    def simulate(self, test_string: Iterable[U], start_state=None) -> T:
         '''
         Simulates the DFA, returning the resulting state. This is the extended
         transition function that accepts a string instead of a single symbol
@@ -202,20 +152,62 @@ class DFA(Generic[T, U]):
 
         return self.simulate(test_string) in self.accept_states
 
+    def get_transition_map(self):
+        '''
+        Returns a dictionary that explcitly maps out the transition function for the DFA
+        '''
+
+        pass
+
+    def get_implicit_transition_map(self):
+        '''
+        This would return a simplified transition map that essentially undoes
+        what the constructor helper for this does, and removes all references
+        to the dead state. Good for printing out and understanding DFAs
+        '''
+
+        # TODO: I must detect all dead states
+        # I feel like maybe this isn't super useful, visualizing is probably better
+        pass
+
+    def visualize(self):
+        '''
+        Can we get graphviz or something to display the DFA?
+        '''
+
+        pass
+
+
+    def get_transition_list(self):
+        '''
+        This would be an interesting function to rename the states so that we
+        could get a super interopable DFA, even for strange states. This would
+        be applicable for some of the conversions between DFA types!
+        '''
+
+        # TODO: should this be implicit or not? A flag you can pass? Two
+        # separate functions?
+        # TODO: ideally we would rename states so that state 1 has a transition
+        # from state 0, and build out using BFS to get interpretable state ids
+
+        pass
+
     @classmethod
     def get_minimal(self):
         '''
-        Returns the unique equivalent DFA with the minimal number of states
+        Returns the unique equivalent DFA with the minimal number of states.
         '''
 
         # TODO: the states should probably frozen sets of original states
         # We can run a renaming function to get a tranisition list if desired
+        # TODO: can this remove states not connected to the start state? (e.g.
+        # the dead state generated)
         pass
 
     def size(self) -> int:
         '''
-        Returns the number of strings accepted by the DFA, or -1 if DFA has
-        infinite size
+        Returns the number of strings accepted by the DFA, or a special enum
+        value if the DFA has infinite size
         '''
 
         raise NotImplementedError
