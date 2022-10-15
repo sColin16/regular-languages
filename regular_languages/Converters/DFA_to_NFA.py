a512a5eab78ab89fde3f86ab612baea174f0bfe3
Colin Siles <colin.siles16@gmail.com>
2022-10-15 16:50:15 -0600

    Implement DFA to NFA conversion

--- regular_languages/Converters/DFA_to_NFA.py
+++ regular_languages/Converters/DFA_to_NFA.py
@@ -1,11 +1,28 @@
+from typing import Set, TypeVar
 from regular_languages import DFA
 from regular_languages import NFA
+from regular_languages.DFAs.dfa import SpecialStates
+from regular_languages.NFAs.nfa import SpecialSymbols
 
-def DFA_to_NFA(dfa: DFA) -> NFA:
+T = TypeVar('T')
+U = TypeVar('U')
+
+def DFA_to_NFA(dfa: DFA[T, U]) -> NFA[T, U]:
     '''
     Constructs an NFA that recognizes the same language as the provided DFA
     '''
 
-    # Wrap the states in a set and we're done!
+    # We ignore the type because this difference should make the type correct
+    states: Set[T] = set(dfa.states).difference({SpecialStates.DEAD}) #type: ignore
+    alphabet = set(dfa.alphabet)
+    start_state = dfa.start_state
+    accept_states = set(dfa.accept_states)
 
-    raise NotImplementedError
+    # Just wrap in a set and handle the empty string as a symbol
+    def transition_function(state: T, symbol: U | SpecialSymbols) -> Set[T]:
+        if symbol == SpecialSymbols.EMPTY:
+            return set()
+
+        return {dfa.transition_function(state, symbol)}
+
+    return NFA(states, alphabet, transition_function, start_state, accept_states)
